2f9a6aefd9a91af6d5bc8baf6c67aa70cc0b21c4
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-12 16:28:03 -0300

    Always use Sandbox

--- app/lib/paypal.ts
+++ app/lib/paypal.ts
@@ -4,9 +4,11 @@ const configureEnvironment = function () {
   const clientId = process.env.PAYPAL_CLIENT_ID!;
   const clientSecret = process.env.PAYPAL_CLIENT_SECRET!;
 
-  return process.env.NODE_ENV === "production"
-    ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
-    : new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+  // return process.env.NODE_ENV === "production"
+  //   ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
+  //   : new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+
+  return new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
 };
 
 const client = function () {
