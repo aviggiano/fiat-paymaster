3af2eb3b4541cde23ca3de4657f001390f04bc28
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-17 20:45:56 -0300

    Build FiatPaymaster

---
+++ smart-contracts/test/FiatPaymaster.ts
@@ -0,0 +1,59 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("FiatPaymaster", function () {
+  async function deployFiatPaymaster() {
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const FiatPaymaster = await ethers.getContractFactory("FiatPaymaster");
+    const fiatPaymaster = await FiatPaymaster.deploy(
+      ethers.constants.AddressZero,
+      "FPUSD",
+      ethers.constants.AddressZero
+    );
+
+    return { fiatPaymaster, owner };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right symbol and name", async function () {
+      const { fiatPaymaster } = await loadFixture(deployFiatPaymaster);
+
+      expect(await fiatPaymaster.symbol()).to.equal("FPUSD");
+      expect(await fiatPaymaster.name()).to.equal("FPUSD");
+    });
+
+    it("Should set the right owner", async function () {
+      const { fiatPaymaster, owner } = await loadFixture(deployFiatPaymaster);
+
+      expect(await fiatPaymaster.owner()).to.equal(owner.address);
+    });
+
+    it("Should not support account creation", async function () {
+      const { fiatPaymaster } = await loadFixture(deployFiatPaymaster);
+
+      expect(await fiatPaymaster.theFactory()).to.equal(
+        ethers.constants.AddressZero
+      );
+    });
+  });
+
+  describe("UserOperation", function () {
+    describe("Conversion", function () {
+      it("Should convert 1 ETH to 1700 USD", async function () {
+        const { fiatPaymaster } = await loadFixture(deployFiatPaymaster);
+
+        expect(await fiatPaymaster.ethToUsd()).to.equal(
+          ethers.constants.MaxUint256
+        );
+
+        await fiatPaymaster.setTokenValueOfEth(ethers.utils.parseEther("1700"));
+        expect(await fiatPaymaster.ethToUsd()).to.equal(
+          ethers.utils.parseEther("1700")
+        );
+      });
+    });
+  });
+});
