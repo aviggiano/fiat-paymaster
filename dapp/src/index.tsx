67ccc5ac173f527bfd91e73fe3fe0f66dfc68b37
delaaxe <1091900+delaaxe@users.noreply.github.com>
2023-03-18 10:16:28 +0100

    Send userOps

--- dapp/src/index.tsx
+++ dapp/src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { App } from "./App";
 import { configureChains, mainnet, goerli, createClient, WagmiConfig } from "wagmi";
 import { infuraProvider } from "wagmi/providers/infura";
+import { App } from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 const { provider, webSocketProvider } = configureChains(
   [mainnet, goerli],
