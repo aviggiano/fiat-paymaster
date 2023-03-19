53aba356b9561777768b49fd5e18db62ed9cae14
delaaxe <1091900+delaaxe@users.noreply.github.com>
2023-03-19 12:20:25 +0100

    Add paymaster owner to backend

---
+++ backend/src/config.ts
@@ -0,0 +1,7 @@
+// goerli:
+export const config = {
+  entryPoint: "0x0576a174D229E3cFA37253523E645A78A0C91B57",
+  simpleAccountFactory: "0x09c58cf6be8E25560d479bd52B4417d15bCA2845",
+  fiatPaymaster: "0x42e723f6Ae6b8CCF83395B09Ea88650381ae23dc",
+  testCounter: "0x6F9641dd4b6Cf822D4cf52ceE753a8910b034827",
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,51 @@
+import "dotenv/config";
+import { BigNumberish, ethers } from "ethers";
 import express from "express";
 import cors from "cors";
+import { config } from "./config";
+
+if (!process.env.GOERLI_PROVIDER_URL) {
+  throw new Error("Provider url not set");
+}
+if (!process.env.PAYMASTER_OWNER_PRIVATE_KEY) {
+  throw new Error("Paymaster owner private key not set");
+}
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_PROVIDER_URL);
+const paymasterSigner = new ethers.Wallet(process.env.PAYMASTER_OWNER_PRIVATE_KEY).connect(provider);
+const abi = ["function owner() external view returns (address)", "function mintTokens(address, uint256)"];
+const fiatPaymaster = new ethers.Contract(config.fiatPaymaster, abi, paymasterSigner);
+
+const mintTokens = async (recipient: string, amount: BigNumberish) => {
+  const response = await fiatPaymaster.mintTokens(recipient, amount);
+  const receipt = await response.wait();
+  return receipt;
+};
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.disable("x-powered-by");
 
+app.get("/config", (req, res) => {
+  res.send(config);
+});
+
 app.get("/health", (req, res) => {
-	console.log("health check");
+  console.log("health check");
   res.send({ status: "ok" });
 });
 
-const port = 3002;
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+(async () => {
+  const paymasterOwner = await fiatPaymaster.owner();
+  const signerAddress = paymasterSigner.address;
+  if (paymasterOwner !== signerAddress) {
+    throw new Error(`Paymaster owner in contract ${paymasterOwner} does not match signer ${signerAddress}`);
+  }
+
+  const port = 3001;
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+})();
