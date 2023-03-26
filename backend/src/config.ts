bd1cb25f2434536ac4ee0fdf4f0e2a94d97e1b98
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-26 07:07:58 -0300

    Update Paypal config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,5 +4,5 @@ export const config = {
   simpleAccountFactory: "0x09c58cf6be8E25560d479bd52B4417d15bCA2845",
   fiatPaymaster: "0x42e723f6Ae6b8CCF83395B09Ea88650381ae23dc",
   testCounter: "0x6F9641dd4b6Cf822D4cf52ceE753a8910b034827",
-  feePercent: 0.02,
+  feePercent: 0.05,
 };
